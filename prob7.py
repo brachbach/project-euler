51e799ba8b95f85fd279f0adce522bb69917ddc4
Ben Rachbach <brachbach@gmail.com>
2018-08-18 14:07:16 -0700

    Minor improvement to prob7

--- prob7.py
+++ prob7.py
@@ -1,23 +1,23 @@
 import math
 
 def find_nth_prime(n):
-    primes_so_far = [2, 3, 5]
-    to_check = 7
+    primes_so_far = [2, 3]
+    num_to_check = 5
     while len(primes_so_far) < n:
         is_prime = True
         for prime in primes_so_far:
-            if prime > math.sqrt(to_check):
+            if prime > math.sqrt(num_to_check):
                 is_prime = True
                 break
-            if to_check % prime == 0:
+            if num_to_check % prime == 0:
                 is_prime = False
                 break
         if is_prime == True:
-            primes_so_far.append(to_check)
-        if (to_check + 1) % 6 == 0:
-            to_check += 2
+            primes_so_far.append(num_to_check)
+        if (num_to_check + 1) % 6 == 0:
+            num_to_check += 2
         else:
-            to_check += 4
+            num_to_check += 4
     return primes_so_far[-1]
 
 print(find_nth_prime(10001))
